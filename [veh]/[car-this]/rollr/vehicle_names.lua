a1ea494e75c79e9025bf88125811331961cb0750
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-13 00:47:25 +0700

    yes

--- [veh]/[car-this]/rollr/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-	AddTextEntry('0x857F902D', 'Rolls-Royce')
-	AddTextEntry('0xE175BEF8', '103EX')
-end)
