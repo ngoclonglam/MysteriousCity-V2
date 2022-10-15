9a506f30cca09025e47b2637e238d411535327a0
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-15 16:17:23 +0700

    yes

--- [qb]/qb-cityhall/config.lua
+++ [qb]/qb-cityhall/config.lua
@@ -4,7 +4,7 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target in
 
 Config.Cityhalls = {
     { -- Cityhall 1
-        coords = vec3(-265.0, -963.6, 31.2),
+        coords = vec3(-551.7, -190.0, 37.22),
         showBlip = true,
         blipData = {
             sprite = 487,
@@ -55,7 +55,7 @@ Config.Peds = {
     -- Cityhall Ped
     {
         model = 'a_m_m_hasjew_01',
-        coords = vec4(-262.79, -964.18, 30.22, 181.71),
+        coords = vec4(-551.7, -190.0, 37.22, 168.18),
         scenario = 'WORLD_HUMAN_STAND_MOBILE',
         cityhall = true,
         zoneOptions = { -- Used for when UseTarget is false
--- [qb]/qb-smallresources/client/ignore.lua
+++ [qb]/qb-smallresources/client/ignore.lua
@@ -82,6 +82,7 @@ CreateThread(function()
 			print('Im inside Car Dealer')
 			ClearAreaOfPeds(-333.08, -918.05, 32.8, 200.0, 1)
 			ClearAreaOfVehicles(-333.08, -918.05, 32.8, 200.0, false, false, false, false, false, false)
+			Wait(5000)
 		end
 
 		if not inside then Wait(1000) end
