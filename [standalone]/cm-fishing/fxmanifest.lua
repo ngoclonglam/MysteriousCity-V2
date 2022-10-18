c2de9cdd049bf8a1f957435c75963b013cff263e
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-18 18:56:16 +0700

    configure fxmanifest of cm-fishing

--- [standalone]/cm-fishing/fxmanifest.lua
+++ [standalone]/cm-fishing/fxmanifest.lua
@@ -28,7 +28,6 @@ files {
 shared_script{
 	'config.lua',
 	'GetFrameworkObject.lua',
-	
 }
 
 
@@ -40,7 +39,7 @@ escrow_ignore {
 	'client/afkmode.lua',
 	'client/minigame.lua',
 	'readme.lua'
-	
+
 }
 
 client_scripts {
@@ -52,12 +51,11 @@ server_scripts {
 	'server/main.lua',
 	'server/PlayerLoaded.lua',
 	'GetFrameworkObject.lua',
-	'@async/async.lua', -- MYSQL ASYNC
 	'@mysql-async/lib/MySQL.lua',  -- MYSQL ASYNC
 }
 dependencies {
 	'fishing-minigame',
-	
+
 }
 
 lua54 'yes'
