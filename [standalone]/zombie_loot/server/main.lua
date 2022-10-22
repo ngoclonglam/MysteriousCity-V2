3b92cd2860ddf09fd507c6888ec69920290233a7
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-23 05:38:46 +0700

    fix done

--- [standalone]/zombie_loot/server/main.lua
+++ [standalone]/zombie_loot/server/main.lua
@@ -2,7 +2,7 @@ local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('zombie_loot:server:lootItem', function(Type)
     local Player = QBCore.Functions.GetPlayer(source)
-
+    local lucky = math.random(1, 100)
     if not Player then return end
 
     if Type == "epic" then
@@ -10,6 +10,11 @@ RegisterNetEvent('zombie_loot:server:lootItem', function(Type)
         TriggerClientEvent('okokNotify:Alert', source, "Hallowen", "Bạn đã kiếm được đồng xu may mắn", 3000, "success")
         TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["lucky_coin"], 'add')
     end
-    TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["pumpkin"], 'add')
-    Player.Functions.AddItem('pumpkin', 1)
+
+    if lucky >= 50 then
+        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items["pumpkin"], 'add')
+        Player.Functions.AddItem('pumpkin', 1)
+    end
+
+    QBCore.Functions.Notify(source, 'Bạn không tìm được thứ gì cả')
 end)
