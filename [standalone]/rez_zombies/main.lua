d6797caf572a9f7d2607b5b591258ebcb3dda890
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-23 04:01:47 +0700

    fix stuff

---
+++ [standalone]/rez_zombies/main.lua
@@ -0,0 +1,87 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+local function searchEpic(model)
+    if IsEntityDead(model) then
+        QBCore.Functions.Progressbar('search_Epic', 'Tìm kiếm đồ', 3000, false, true, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true
+          }, {
+            animDict = 'random@domestic',
+            animName = 'pickup_low',
+            flags = 16
+          }, {}, {}, function()
+            -- Done
+            local lucky = math.random(1, 100)
+            local ammo = math.random(36, 72)
+            ClearPedSecondaryTask(PlayerPedId())
+            local entity = GetEntityModel(model)
+            SetEntityAsNoLongerNeeded(entity)
+            SetModelAsNoLongerNeeded(model)
+            DeleteEntity(entity)
+            if lucky >= 20 then
+                GiveWeaponToPed(PlayerPedId(), 'weapon_pistol', ammo, true, true)
+                exports['okokNotify']:Alert('Halloween', 'Bạn đã lấy được một cây súng lục', 3000, 'success')
+            end
+            TriggerServerEvent('rez_zombies:server:lootItem', 'Epic')
+        end)
+    end
+end
+
+local function searchItem(entity)
+    if IsEntityDead(entity) then
+        QBCore.Functions.Progressbar('search_Item', 'Tìm kiếm đồ', 3000, false, true, {
+            disableMovement = true,
+            disableCarMovement = true,
+            disableMouse = false,
+            disableCombat = true
+          }, {
+            animDict = 'random@domestic',
+            animName = 'pickup_low',
+            flags = 16
+          }, {}, {}, function()
+            -- Done
+            local lucky = math.random(1, 100)
+            local ammo = math.random(24, 48)
+            ClearPedSecondaryTask(PlayerPedId())
+            local model = GetEntityModel(entity)
+            SetEntityAsNoLongerNeeded(model)
+            SetModelAsNoLongerNeeded(entity)
+            DeleteEntity(model)
+            if lucky >= 40 then
+                GiveWeaponToPed(PlayerPedId(), 'weapon_pistol', ammo, true, true)
+                exports['okokNotify']:Alert('Halloween', 'Bạn đã lấy được một cây súng lục', 3000, 'success')
+            end
+            TriggerServerEvent('rez_zombies:server:lootItem', 'normal')
+        end)
+    end
+end
+
+CreateThread(function()
+    exports['qb-target']:AddTargetModel(Config.MutantModels, {
+        options = {
+            {
+                icon = "fas fa-sack-dollar",
+                label = "Kiếm đồ",
+                action = function(entity)
+                    searchEpic(entity)
+                end,
+            },
+        },
+        distance = 2.5,
+    })
+
+    exports['qb-target']:AddTargetModel(Config.PedModels, {
+        options = {
+            {
+                icon = "fas fa-sack-dollar",
+                label = "Kiếm đồ",
+                action = function(entity)
+                    searchItem(entity)
+                end,
+            },
+        },
+        distance = 2.5,
+    })
+end)
