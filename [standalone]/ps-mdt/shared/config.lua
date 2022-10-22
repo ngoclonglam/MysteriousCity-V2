898822d0df05387201655516f110e8786f97bab6
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-22 22:27:45 +0700

    yes

--- [standalone]/ps-mdt/shared/config.lua
+++ [standalone]/ps-mdt/shared/config.lua
@@ -18,7 +18,7 @@ This will only check plates of vehicles that have been occupied by a player
 
 Config.OnlyShowOnDuty = true
 
-Config.Fuel = "lj-fuel" -- "LegacyFuel", "lj-fuel"
+Config.Fuel = "ps-fuel" -- "LegacyFuel", "lj-fuel"
 
 Config.PenalCodeTitles = {
     [1] = 'OFFENSES AGAINST PERSONS',
