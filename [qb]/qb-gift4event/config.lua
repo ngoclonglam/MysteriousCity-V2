cd75d3cc0fc8fa00b46733bb727ff29fb10269b2
rpbagaming <ngoclonglam@yahoo.com.vn>
2022-10-25 00:49:47 +0700

    Yes

--- [qb]/qb-gift4event/config.lua
+++ [qb]/qb-gift4event/config.lua
@@ -3,14 +3,14 @@ Config = {}
 Config.Gifts = {
     ["gift-newbie"] = { -- Key of row should be matched with item defined in shared/items.lua
         { type = "car", name = "bison", luck = { min = 1, max = 30} },
-        { type = "car", name = "bobcatxl", luck = { min = 31, max = 60} },
-        { type = "car", name = "burrito3", luck = { min = 61, max = 90} },
-        { type = "car", name = "minivan2", luck = { min = 91, max = 100} },
+        { type = "car", name = "bobcatxl", luck = { min = 31, max = 90} },
+        { type = "car", name = "burrito3", luck = { min = 91, max = 150} },
+        { type = "car", name = "minivan2", luck = { min = 181, max = 200} },
     },
     ["halloween_gift"] = {
-        { type = "car", name = "pariah", luck = {min = 1, max = 2} },
-        { tyype = "car", name = "scorcher", luck = {min = 3, max = 10} },
-        { type = "item", name = "renamecard", luck = {min = 11, max = 30} },
+        { type = "car", name = "pariah", luck = {min = 1, max = 1} },
+        { type = "car", name = "scorcher", luck = {min = 2, max = 5} },
+        { type = "item", name = "renamecard", luck = {min = 6, max = 30} },
         { type = "item", name = "phonecard", luck = {min = 31, max = 50} },
         { type = "item", name = "carcard", luck = {min = 51, max = 70} },
         { type = "money", amount = math.random(100, 500), luck = {min = 71, max = 200} },
